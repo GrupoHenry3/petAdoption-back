4dbd133e24d788e492574651df9510ef9a55b7e4
Angel <230014780+j14i@users.noreply.github.com>
2025-09-03 18:05:07 -0700

    Configuracion de Prisma y Swagger (#1)
    * feat: integrate Prisma with LibSQL adapter and configure database schema
    
    - Added Prisma dependencies to package.json.
    - Created prisma.config.ts to configure Prisma with LibSQL adapter.
    - Added schema.prisma defining models for User, Organization, Animal, Breed, Adoption, and Species.
    - Implemented PrismaService to manage database connections.
    
    * feat: add Swagger documentation and validation pipes to the application
    
    * feat: move modules out of 'modules' folder, delete unnecessary DTO and Entity files
    
    Signed-off-by: Angel <230014780+j14i@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Angel <230014780+j14i@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder().setTitle('Pet Adoption').setVersion('1.0').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true}))
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
+
 bootstrap();
