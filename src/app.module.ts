c6b62ff3b29039a920b86459d5d9559067235212
Cristian <liccristian147@gmail.com>
2025-09-13 12:31:25 -0300

    feature/pets - animal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { AnimalModule } from './modules/animals/animal.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, AnimalModule],
 })
 export class AppModule {}
