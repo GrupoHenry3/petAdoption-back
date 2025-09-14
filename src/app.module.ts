e1e5fd49cc99074c1f70e37bd9b90fefb5c39c49
Angel <230014780+j14i@users.noreply.github.com>
2025-09-13 18:57:56 -0700

    Update schema and disable animals (#6)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { AnimalModule } from './modules/animals/animal.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, AnimalModule],
+  imports: [UsersModule, AuthModule],
 })
 export class AppModule {}
