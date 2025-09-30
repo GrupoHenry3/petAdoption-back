edf39d19787d5c57af82d6e189745ffb513ffae2
Angel <230014780+j14i@users.noreply.github.com>
2025-09-29 23:37:17 -0400

    Fix mail module import in shelters module

--- src/modules/shelters/shelters.module.ts
+++ src/modules/shelters/shelters.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { SheltersController } from './shelters.controller';
 import { SheltersService } from './shelters.service';
+import { MailModule } from '../mail/mail.module';
+import { MailService } from '../mail/mail.service';
 
 @Module({
+  imports: [MailModule],
   controllers: [SheltersController],
-  providers: [SheltersService, PrismaService],
+  providers: [SheltersService, PrismaService, MailService],
 })
 export class SheltersModule {}
