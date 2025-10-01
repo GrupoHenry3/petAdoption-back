7061d707d8ddfa829774ec1628f105d9cf3c617d
Angel <230014780+j14i@users.noreply.github.com>
2025-10-01 17:28:06 -0400

    Security tweaks

--- src/modules/users/user.interface.ts
+++ src/modules/users/user.interface.ts
@@ -1,18 +1,4 @@
 import { UserType } from '@prisma/client';
 
-export interface IUser {
-  id: string;
-  fullName?: string;
-  email: string;
-  password: string;
-  googleID?: string;
-  phoneNumber?: string;
-  city?: string;
-  country?: string;
-  avatarURL?: string;
-  userType: UserType;
-  siteAdmin: boolean;
-  isActive: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-}
+export class User {}
+export class CurrentUser {}
