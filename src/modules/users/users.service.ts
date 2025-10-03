9859bf33edf18c2104c95c74c2625f5b8574f486
Angel <230014780+j14i@users.noreply.github.com>
2025-10-03 00:53:01 -0400

    Disable mail calls

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -51,7 +51,7 @@ export class UsersService {
       const user = await this.prisma.user.create({ data: newUser });
       this.logger.log('User created successfully');
       
-      await this.mailService.signUpConfirmation(user.fullName, user.email);
+      // await this.mailService.signUpConfirmation(user.fullName, user.email);
 
       return {
         statusCode: HttpStatus.CREATED,
