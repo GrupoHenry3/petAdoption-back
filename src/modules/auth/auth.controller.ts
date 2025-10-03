cb2a88ad171753ee1472c6b6cfdb30af044cfae1
Angel <230014780+j14i@users.noreply.github.com>
2025-10-03 12:48:51 -0400

    sigh

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -20,8 +20,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: true,
   sameSite: 'none' as const,
-  maxAge: 60 * 60 * 1000,
-  domain: `${process.env.FRONTEND_URL}`,
+  maxAge: 60 * 60 * 1000
 };
 @Controller('auth')
 export class AuthController {
